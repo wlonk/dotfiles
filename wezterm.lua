f21630c740696288e0f601c451046c23ce093335
Kit La Touche <kit@transneptune.net>
2026-09-15 15:09:09 -0400

    Update WezTerm config
    Made fonts Bold (they were too scrawny otherwise) and changed colour
    scheme (to look a bit more familiar).

--- wezterm.lua
+++ wezterm.lua
@@ -12,9 +12,9 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 14
-config.font = wezterm.font 'Dank Mono'
+config.font = wezterm.font('Dank Mono', { weight = 'Bold' })
 -- config.color_scheme_dirs = { '/Users/kit/Dropbox/For the Laptop/iTerm2 colors' }
-config.color_scheme = 'Ayu Mirage'
+config.color_scheme = 'Hybrid (terminal.sexy)'
 
 config.hide_tab_bar_if_only_one_tab = true
 
