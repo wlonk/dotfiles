4820c43fa1163a0a5c596c460f6d463a24223b47
Kit La Touche <kit@transneptune.net>
2026-09-15 15:18:01 -0400

    Add a cool gradient background to my terminal

--- wezterm.lua
+++ wezterm.lua
@@ -14,9 +14,60 @@ config.initial_rows = 28
 config.font_size = 14
 config.font = wezterm.font('Dank Mono', { weight = 'Bold' })
 -- config.color_scheme_dirs = { '/Users/kit/Dropbox/For the Laptop/iTerm2 colors' }
-config.color_scheme = 'Hybrid (terminal.sexy)'
+config.color_scheme = 'Fishbone (terminal.sexy)'
 
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_gradient = {
+  -- Can be "Vertical" or "Horizontal".  Specifies the direction
+  -- in which the color gradient varies.  The default is "Horizontal",
+  -- with the gradient going from left-to-right.
+  -- Linear and Radial gradients are also supported; see the other
+  -- examples below
+  orientation = 'Vertical',
+
+  -- Specifies the set of colors that are interpolated in the gradient.
+  -- Accepts CSS style color specs, from named colors, through rgb
+  -- strings and more
+  colors = {
+    '#0f0c29',
+    '#302b63',
+    '#24243e',
+  },
+
+  -- Instead of specifying `colors`, you can use one of a number of
+  -- predefined, preset gradients.
+  -- A list of presets is shown in a section below.
+  -- preset = "Warm",
+
+  -- Specifies the interpolation style to be used.
+  -- "Linear", "Basis" and "CatmullRom" as supported.
+  -- The default is "Linear".
+  interpolation = 'Linear',
+
+  -- How the colors are blended in the gradient.
+  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
+  -- The default is "Rgb".
+  blend = 'Rgb',
+
+  -- To avoid vertical color banding for horizontal gradients, the
+  -- gradient position is randomly shifted by up to the `noise` value
+  -- for each pixel.
+  -- Smaller values, or 0, will make bands more prominent.
+  -- The default value is 64 which gives decent looking results
+  -- on a retina macbook pro display.
+  -- noise = 64,
+
+  -- By default, the gradient smoothly transitions between the colors.
+  -- You can adjust the sharpness by specifying the segment_size and
+  -- segment_smoothness parameters.
+  -- segment_size configures how many segments are present.
+  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
+  -- 1.0 is a soft edge.
+
+  -- segment_size = 11,
+  -- segment_smoothness = 0.0,
+}
 
 -- Finally, return the configuration to wezterm:
 return config
