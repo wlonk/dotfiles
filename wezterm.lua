0e8e7dd8109448f555457e9765a4fbe09ffc189f
Kit La Touche <kit@transneptune.net>
2026-09-15 16:41:38 -0400

    Use a fancier WezTerm colour scheme

--- wezterm.lua
+++ wezterm.lua
@@ -14,7 +14,7 @@ config.initial_rows = 28
 config.font_size = 14
 config.font = wezterm.font('Dank Mono', { weight = 'Bold' })
 -- config.color_scheme_dirs = { '/Users/kit/Dropbox/For the Laptop/iTerm2 colors' }
-config.color_scheme = 'Fishbone (terminal.sexy)'
+config.color_scheme = 'Dark Violet (base16)'
 
 config.hide_tab_bar_if_only_one_tab = true
 
